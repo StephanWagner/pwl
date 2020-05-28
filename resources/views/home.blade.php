354694ee2c38b2ff6b48b242c3c85fae81b8a8a6
Stephan Wagner <stephanwagner.me@gmail.com>
2020-05-28 17:39:36 +0200

    Add initial HTML on home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>vaulty</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<meta name="author" content="Stephan Wagner, stephanwagner.me@gmail.com">
+<meta name="description" content="">
+<link rel="icon" type="image/png" href="/img/favicon/favicon-stephan-512.png">
+<link rel="apple-touch-icon" type="image/png" href="/apple-touch-icon.png">
+<link rel="manifest" href="/manifest.json">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+
+<body>
+
+<header>
+// HEADER
+</header>
+
+<main>
+// MAIN
+
+--<br><br>
+
+<a href="{{ url('/') }}">TEST</a>
+
+<br><br>--
+
+</main>
+
+<footer>
+// FOOTER
+</footer>
+
+<script src="/js/main.js"></script>
+
+</body>
+</html>
