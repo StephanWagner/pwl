de47c532cb62a2621eab6b43c0c2715f47a5a566
Stephan Wagner <stephanwagner.me@gmail.com>
2020-05-28 23:30:59 +0200

    Add footer styles, add attribution

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,11 +1,14 @@
 </main>
 
-<footer>
-	<div class="container">
-		<nav class="footer-nav" data-locale="{{ App::getLocale() }}">
+<footer class="footer__wrapper">
+	<div class="footer__container container">
+		<div class="footer__attribution">
+			By <a href="https://stephanwagner.me" target="_blank">Stephan Wagner</a>
+		</div>
+		<nav class="footer__nav" data-locale="{{ App::getLocale() }}">
 			<ul>
-				<li><span{!! App::isLocale('en') ? '' : ' onclick="setLocale(\'en\')"' !!}>English</span></li>
-				<li><span{!! App::isLocale('de') ? '' : ' onclick="setLocale(\'de\')"' !!}>Deutsch</span></li>
+				<li><span class="link"{!! App::isLocale('en') ? '' : ' onclick="setLocale(\'en\')"' !!}>English</span></li>
+				<li><span class="link"{!! App::isLocale('de') ? '' : ' onclick="setLocale(\'de\')"' !!}>Deutsch</span></li>
 			</ul>
 		</nav>
 	</div>
